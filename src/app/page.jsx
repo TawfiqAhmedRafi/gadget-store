32fe62dfaf9a3144b803b6da5119e824a2cee1c0
TawfiqAhmedRafi <rafi70722@gmail.com>
2026-01-18 22:22:11 +0600

    hero and about section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,12 @@
+import AboutSection from "@/components/landing/AboutSection";
+import HeroSection from "@/components/landing/HeroSection";
 
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     
+ 
+     <AboutSection></AboutSection>
     </div>
   );
 }
